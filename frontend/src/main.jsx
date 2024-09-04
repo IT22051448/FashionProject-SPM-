cf4721dc159bdc1eb1b848d30e4319d369b68919
Manilka Navod <manilka.projects@gmail.com>
2024-09-04 20:46:33 +0530

    changing directory structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from "./store/store.js";
+import store from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
