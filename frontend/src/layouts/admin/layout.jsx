bdbe3dc4ef0200cb8dfa41e21716948fc856cfd4
Manilka Navod <manilka.projects@gmail.com>
2024-09-05 21:14:22 +0530

    admin dashbaord setup

--- frontend/src/layouts/admin/layout.jsx
+++ frontend/src/layouts/admin/layout.jsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "./Components/sidebar";
 import AdminHeader from "./Components/header";
+import { useState } from "react";
 
 const AdminLayout = () => {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
   return (
     <div className="flex min-h-screen w-full ">
       {/* Side Bar */}
-      <AdminSidebar />
+      <AdminSidebar open={openSidebar} setOpen={setOpenSidebar} />
       <div className="flex flex-1 flex-col">
         {/* Header */}
-        <AdminHeader />
-        <main className="flex-1 flex bg-muted/40 p-4 md:p-6">
+        <AdminHeader setOpen={setOpenSidebar} />
+        <main className="flex-1 flex-col bg-muted/40 p-4 md:p-6">
           <Outlet />
         </main>
       </div>
--- frontend/src/pages/admin/products/products.jsx
+++ frontend/src/pages/admin/products/products.jsx
@@ -1,5 +1,35 @@
+import { Fragment, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+
 const AdminProducts = () => {
-  return <div>AdminProducts</div>;
+  const [openCreateProductDialog, setOpenCreateProductDialog] = useState(false);
+
+  return (
+    <Fragment>
+      <div className="mb-5 w-full flex justify-end">
+        <Button onClieck={() => setOpenCreateProductDialog(true)}>
+          Add New Product
+        </Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <Sheet
+        open={openCreateProductDialog}
+        onOpenChange={setOpenCreateProductDialog}
+      >
+        <SheetContent side="right" className="overflow-auto">
+          <SheetHeader>
+            <SheetTitle>Add New Product</SheetTitle>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </Fragment>
+  );
 };
 
 export default AdminProducts;
