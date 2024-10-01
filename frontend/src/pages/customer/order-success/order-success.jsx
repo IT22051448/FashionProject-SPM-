d60d10cad263b3330123984649712786d7a08f1d
Manilka Navod <manilka.projects@gmail.com>
2024-10-01 22:35:07 +0530

    order-success mods

--- frontend/src/pages/customer/order-success/order-success.jsx
+++ frontend/src/pages/customer/order-success/order-success.jsx
@@ -6,6 +6,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { getOrder, resetPlacedOrderId } from "@/redux/orderSlice";
+import { Label } from "@radix-ui/react-dropdown-menu";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -31,7 +32,32 @@ const OrderSuccess = () => {
             Thank you for your order. Your order has been placed successfully.
           </CardDescription>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent>
+          <div className="grid gap-6">
+            <div className="grid gap-2">
+              <div className="flex mt-6 items-center justify-between">
+                <p className="font-medium">Order ID</p>
+                <Label>{order?._id}</Label>
+              </div>
+              <div className="flex mt-2 items-center justify-between">
+                <p className="font-medium">Order Date</p>
+                <Label>{order?.orderDate.split("T")[0]}</Label>
+              </div>
+              <div className="flex mt-2 items-center justify-between">
+                <p className="font-medium">Order Price</p>
+                <Label>${order?.totalAmount}</Label>
+              </div>
+              <div className="flex mt-2 items-center justify-between">
+                <p className="font-medium">Payment method</p>
+                <Label>{order?.paymentMethod}</Label>
+              </div>
+              <div className="flex mt-2 items-center justify-between">
+                <p className="font-medium">Payment Status</p>
+                <Label>{order?.paymentStatus}</Label>
+              </div>
+            </div>
+          </div>
+        </CardContent>
       </Card>
     </div>
   );
