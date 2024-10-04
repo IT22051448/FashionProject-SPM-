80baeae2b30605578fc49b42e808a724c3def9fa
IT22051448 <IT22051448@my.sliit.lk>
2024-10-04 23:20:10 +0530

    idk

--- backend/src/api/controllers/referralController.js
+++ backend/src/api/controllers/referralController.js
@@ -47,7 +47,6 @@ exports.createReferral = async (req, res) => {
     const referralLink = `http://localhost:5173/auth/signup?referralToken=${referralToken}`;
 
     const mailOptions = {
-      from: process.env.EMAIL_USER,
       to: referredEmail,
       subject: `You’ve been referred to join our loyalty program!`,
       text: `Hello,
